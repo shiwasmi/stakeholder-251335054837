cd4b70100cc54b4a2c0ac060d17c04d1d40e1923
shiwasmi <sagarchattar@gmail.com>
2026-05-12 15:31:29 +0530

    Terraform  Code

--- resources.tf
+++ resources.tf
@@ -22,24 +22,24 @@ module "myec2" {
   ec2_instance_type = "t3.micro"
 }
 
-# module "my_vpc" {
-#   source     = "git::https://github.com/shiwasmi/aws_tf_modules.git//modules/vpc?ref=v1.8.0"
-#   vpc_name   = "sagar-vpc"
-#   cidr_block = "10.0.0.0/16"
+module "my_vpc" {
+  source     = "git::https://github.com/shiwasmi/aws_tf_modules.git//modules/vpc?ref=v1.8.0"
+  vpc_name   = "sagar-vpc"
+  cidr_block = "10.0.0.0/16"
 
-#   public_subnets  = ["10.0.1.0/24", "10.0.2.0/24"]
-#   private_subnets = ["10.0.101.0/24", "10.0.102.0/24"]
-# }
+  public_subnets  = ["10.0.1.0/24", "10.0.2.0/24"]
+  private_subnets = ["10.0.101.0/24", "10.0.102.0/24"]
+}
 
-# module "mysql_db" {
-#   source                 = "git::https://github.com/shiwasmi/aws_tf_modules.git//modules/mysqldb?ref=v1.8.0"
-#   db_name                = var.db_name
-#   db_username            = var.db_username
-#   db_password            = var.db_password
-#   instance_class         = var.instance_class
-#   vpc_security_group_ids = var.vpc_security_group_ids
-#   subnet_ids = [
-#     "subnet-08e18b60ea182c880",
-#     "subnet-0809eaeaacc4d4ce0"
-#   ]
-# }
+module "mysql_db" {
+  source                 = "git::https://github.com/shiwasmi/aws_tf_modules.git//modules/mysqldb?ref=v1.8.0"
+  db_name                = var.db_name
+  db_username            = var.db_username
+  db_password            = var.db_password
+  instance_class         = var.instance_class
+  vpc_security_group_ids = var.vpc_security_group_ids
+  subnet_ids = [
+    "subnet-08e18b60ea182c880",
+    "subnet-0809eaeaacc4d4ce0"
+  ]
+}
